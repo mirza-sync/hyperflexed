f4a0803f6e0968b467a637b810b74bca1ddf226b
mirza-sync <mirzasyahmi123@gmail.com>
2024-02-05 23:57:28 +0800

    Add function to create  tiles

--- 5-staggered-grid-ripple/index.js
+++ 5-staggered-grid-ripple/index.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+let columns = Math.floor(document.body.clientWidth / 50),
+  rows = Math.floor(document.body.clientHeight / 50);
+
+const createTile = index => {
+  const tile = document.createElement("div");
+  tile.classList.add("tile");
+  return tile;
+}
+
+const createTiles = quantity => {
+  Array.from(Array(quantity)).map((tile, index) => {
+    wrapper.appendChild(createTile(index));
+  })
+}
+
+createTiles(columns * rows);
