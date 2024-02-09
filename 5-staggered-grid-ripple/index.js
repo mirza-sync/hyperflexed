b3f9dc1f84f99e94a30b52106434981d2ec8055f
mirza-sync <mirzasyahmi123@gmail.com>
2024-02-09 23:56:22 +0800

    Add responsive dynamic auto-grids awesomeness

--- 5-staggered-grid-ripple/index.js
+++ 5-staggered-grid-ripple/index.js
@@ -21,7 +21,14 @@ const createGrid = () => {
   columns = Math.floor(document.body.clientWidth / 50);
   rows = Math.floor(document.body.clientHeight / 50);
 
+  console.log(columns, rows);
+
+  wrapper.style.setProperty('--columns', columns);
+  wrapper.style.setProperty('--rows', rows);
+
   createTiles(columns * rows);
 }
+
+createGrid();
 
 window.onresize = () => createGrid();
