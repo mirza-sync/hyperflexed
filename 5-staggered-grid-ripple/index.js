80863300c75e49ab3471c7a794007020234d38d8
mirza-sync <mirzasyahmi123@gmail.com>
2024-03-18 01:01:48 +0800

    Change bacground color on each click

--- 5-staggered-grid-ripple/index.js
+++ 5-staggered-grid-ripple/index.js
@@ -2,10 +2,33 @@ const wrapper = document.getElementById("tiles");
 
 let columns = Math.floor(document.body.clientWidth / 50),
   rows = Math.floor(document.body.clientHeight / 50);
+const colors = [
+  "rgb(100, 30, 5)",
+  "rgb(9, 32, 7)",
+  "rgb(50, 100, 200)",
+  "rgb(255, 0, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 0, 255)",
+  "rgb(150, 75, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(255, 0, 255)",
+  "rgb(0, 255, 255)"
+];
+let count = -1
+
+const handleOnClick = (index) => {
+  count = count + 1
+
+  anime({
+    targets: ".tile",
+    backgroundColor: colors[count % (colors.length - 1)],
+  })
+}
 
 const createTile = index => {
   const tile = document.createElement("div");
   tile.classList.add("tile");
+  tile.onclick = (e) => handleOnClick(index)
   return tile;
 }
 
