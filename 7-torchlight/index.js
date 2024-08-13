7a3738bee4ac0dd5fc2d1291cca03b1b94132a9f
mirza-sync <mirzasyahmi123@gmail.com>
2024-08-13 23:30:35 +0800

    Fill box with random string on mouse move

--- 7-torchlight/index.js
+++ 7-torchlight/index.js
@@ -1,1 +1,13 @@
-console.log("Hello Torchlight");
+const chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+
+const randomChar = () => {
+  return chars[Math.floor(Math.random() * chars.length)];
+};
+const randomString = (length) =>
+  Array.from(Array(length)).map(randomChar).join("");
+
+const box = document.getElementById("letterbox");
+
+window.onmousemove = (e) => {
+  box.innerText = randomString(1500);
+};
