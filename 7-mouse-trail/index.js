8870b44ffae612793277dbcf2ed2cdb437ad6962
mirza-sync <mirzasyahmi123@gmail.com>
2024-09-15 00:42:03 +0800

    Make trailer follows cursor

--- 7-mouse-trail/index.js
+++ 7-mouse-trail/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const trailer = document.getElementById("trailer");
+window.onmousemove = (e) => {
+  const x = e.clientX - trailer.offsetWidth / 2;
+  const y = e.clientY - trailer.offsetHeight / 2;
+
+  console.log(x, y);
+  trailer.style.transform = `translate(${x}px ${y}px)`;
+};
