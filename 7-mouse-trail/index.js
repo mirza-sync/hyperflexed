0a4feefc2ef48756cc27dceb763456ddba16ab62
mirza-sync <mirzasyahmi123@gmail.com>
2024-09-16 23:01:16 +0800

    Animate mouse move instead of directly follows cursor

--- 7-mouse-trail/index.js
+++ 7-mouse-trail/index.js
@@ -3,6 +3,12 @@ window.onmousemove = (e) => {
   const x = e.clientX - trailer.offsetWidth / 2;
   const y = e.clientY - trailer.offsetHeight / 2;
 
-  console.log(x, y);
-  trailer.style.transform = `translate(${x}px, ${y}px)`;
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px)`,
+  };
+
+  trailer.animate(keyframes, {
+    duration: 800,
+    fill: "forwards",
+  });
 };
