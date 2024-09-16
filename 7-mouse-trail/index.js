d86c50e831ada4e045477aa9a146e7ed95877a53
mirza-sync <mirzasyahmi123@gmail.com>
2024-09-16 23:39:14 +0800

    Make mouse shows different icon based on hovered item

--- 7-mouse-trail/index.js
+++ 7-mouse-trail/index.js
@@ -1,14 +1,36 @@
 const trailer = document.getElementById("trailer");
-window.onmousemove = (e) => {
+
+const animateTrailer = (e, interacting) => {
   const x = e.clientX - trailer.offsetWidth / 2;
   const y = e.clientY - trailer.offsetHeight / 2;
 
   const keyframes = {
-    transform: `translate(${x}px, ${y}px)`,
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 5 : 1})`,
   };
 
   trailer.animate(keyframes, {
     duration: 800,
     fill: "forwards",
   });
+};
+
+const getTrailerClass = (type) => {
+  switch (type) {
+    case "video":
+      return "▶";
+
+    default:
+      return "👀";
+  }
+};
+
+window.onmousemove = (e) => {
+  const interactable = e.target.closest(".interactable");
+  const interacting = interactable !== null;
+
+  animateTrailer(e, interacting);
+
+  if (interacting) {
+    trailer.innerHTML = getTrailerClass(interactable.dataset.type);
+  }
 };
